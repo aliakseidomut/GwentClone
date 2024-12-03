b6c32f36c228b48923df49c18051072375dc72ef
Aliaksei Domut <alexedomut@gmail.com>
2024-12-04 02:05:10 +0300

    feat: return to main-page after game

--- client/src/pages/PlayPage/PlayPage.tsx
+++ client/src/pages/PlayPage/PlayPage.tsx
@@ -1,62 +1,87 @@
-import React, { useEffect, useState } from 'react';
-import './PlayPage.scss';
-import { AppDispatch, RootState } from '../../redux/store';
-import { useDispatch, useSelector } from 'react-redux';
-import { LoadingPage } from '../LoadingPage/LoadingPage';
-import { getRoom } from '../../redux/features/room/roomSlice';
-import { UserInfo } from '../../components/UserInfo/UserInfo';
-import { io } from 'socket.io-client';
-import { UserGameState } from '../../types/interfaces';
-import { CardsOutPlay } from '../../components/CardsOutPlay/CardsOutPlay';
-import { CardsOnTable } from '../../components/CardsOnTable/CardsOnTable';
+import React, { useEffect, useState } from "react";
+import "./PlayPage.scss";
+import { AppDispatch, RootState } from "../../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { LoadingPage } from "../LoadingPage/LoadingPage";
+import { getRoom } from "../../redux/features/room/roomSlice";
+import { UserInfo } from "../../components/UserInfo/UserInfo";
+import { io } from "socket.io-client";
+import { UserGameState } from "../../types/interfaces";
+import { CardsOutPlay } from "../../components/CardsOutPlay/CardsOutPlay";
+import { CardsOnTable } from "../../components/CardsOnTable/CardsOnTable";
+import { useNavigate } from "react-router-dom";
 
-const socket = io('http://localhost:3002');
+const socket = io("http://localhost:3002");
 
 export const PlayPage: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const decks = useSelector((state: RootState) => state.decks.decks);
-  const currentDeckNum = useSelector((state: RootState) => state.decks.currentDeckNum) || 0;
+  const currentDeckNum =
+    useSelector((state: RootState) => state.decks.currentDeckNum) || 0;
   const roomId = useSelector((state: RootState) => state.room.room?._id);
   const user = useSelector((state: RootState) => state.auth.user);
+  const navigate = useNavigate();
 
   const [gameState, setGameState] = useState(null);
 
-  function getImgUrl(fileName: string){
-    const ext = '.png'
-    const imgUrl = new URL(`../../assets/images/cardsImages/${fileName}${ext}`, import.meta.url).href
-    return imgUrl
+  function getImgUrl(fileName: string) {
+    const ext = ".png";
+    const imgUrl = new URL(
+      `../../assets/images/cardsImages/${fileName}${ext}`,
+      import.meta.url
+    ).href;
+    return imgUrl;
   }
 
   useEffect(() => {
     dispatch(getRoom());
-    if (!localStorage.getItem('gameState')){
-      socket.emit('join', {
+    if (!localStorage.getItem("gameState")) {
+      socket.emit("join", {
         roomId,
         userData: {
           username: user?.username,
           deck: decks[currentDeckNum],
         },
-    });
+      });
     }
   }, [currentDeckNum, decks, dispatch, roomId, user]);
 
   useEffect(() => {
-    const savedGameState = localStorage.getItem('gameState');
+    const savedGameState = localStorage.getItem("gameState");
     if (savedGameState) {
       setGameState(JSON.parse(savedGameState));
     }
   }, []);
 
   useEffect(() => {
-    socket.on('gameState', ({ gameState }) => {
-      const userGameState: UserGameState = gameState.filter(el => el.username === user?.username)[0];
-      const opponentGameState: UserGameState = gameState.filter(el => el.username !== user?.username)[0];
-      
-      setGameState({user: userGameState, opponent: opponentGameState});
-      
-      localStorage.setItem('gameState', JSON.stringify({user: userGameState, opponent: opponentGameState}));
-    })
+    socket.on("gameState", ({ gameState }) => {
+      const userGameState: UserGameState = gameState.filter(
+        (el) => el.username === user?.username
+      )[0];
+      const opponentGameState: UserGameState = gameState.filter(
+        (el) => el.username !== user?.username
+      )[0];
+
+      setGameState({ user: userGameState, opponent: opponentGameState });
+
+      localStorage.setItem(
+        "gameState",
+        JSON.stringify({ user: userGameState, opponent: opponentGameState })
+      );
+    });
   }, [user]);
+
+  useEffect(() => {
+    socket.on("gameOver", ({ winner }) => {
+      setGameState(null);
+      localStorage.removeItem("gameState");
+      navigate("/main");
+    });
+
+    return () => {
+      socket.off("gameOver");
+    };
+  }, [navigate]);
 
   if (!gameState || !gameState.user || !gameState.opponent) {
     return <LoadingPage />;
@@ -65,38 +90,55 @@ export const PlayPage: React.FC = () => {
   return (
     <div className="PlayPage">
       <div className="info">
-        <UserInfo user={gameState.opponent}/>
-        
-        <div className='specialCards'>
-          {
-            [...gameState.user.cardsOnTable, ...gameState.opponent.cardsOnTable].filter(el => el.type === 'SpecialCard').map(el => <img src={getImgUrl(el.name.split(' ').join(''))} />)
+        <UserInfo user={gameState.opponent} />
 
-          }
+        <div className="specialCards">
+          {[...gameState.user.cardsOnTable, ...gameState.opponent.cardsOnTable]
+            .filter((el) => el.type === "SpecialCard")
+            .map((el) => (
+              <img src={getImgUrl(el.name.split(" ").join(""))} />
+            ))}
         </div>
 
         <UserInfo user={gameState.user} />
       </div>
-      
+
       <div className="table">
-        <CardsOnTable userStatus='opponent' cardsOnTable={gameState.opponent.cardsOnTable}  />
-        <CardsOnTable userStatus='user' cardsOnTable={gameState.user.cardsOnTable} />
-        <div className='currentCards'>
-          {
-            gameState.user.currentCards.map((el, i) => 
-            <img 
-              src={getImgUrl(el.name.split(' ').join(''))} 
-              className='currentCardsCard' 
+        <CardsOnTable
+          userStatus="opponent"
+          cardsOnTable={gameState.opponent.cardsOnTable}
+        />
+        <CardsOnTable
+          userStatus="user"
+          cardsOnTable={gameState.user.cardsOnTable}
+        />
+        <div className="currentCards">
+          {gameState.user.currentCards.map((el, i) => (
+            <img
+              src={getImgUrl(el.name.split(" ").join(""))}
+              className="currentCardsCard"
               onClick={() => {
-                socket.emit('pushCard', {username: gameState.user.username, cardIndex: i});
+                socket.emit("pushCard", {
+                  username: gameState.user.username,
+                  cardIndex: i,
+                });
               }}
             />
-          )}
+          ))}
         </div>
       </div>
-      
+
       <div className="cardsOutPlay">
-        <CardsOutPlay cardsInDeck={gameState.opponent.cardsInDeck} lostCards={gameState.opponent.lostCards} fraction={gameState.opponent.fraction} />
-        <CardsOutPlay cardsInDeck={gameState.user.cardsInDeck} lostCards={gameState.user.lostCards} fraction={gameState.user.fraction} />
+        <CardsOutPlay
+          cardsInDeck={gameState.opponent.cardsInDeck}
+          lostCards={gameState.opponent.lostCards}
+          fraction={gameState.opponent.fraction}
+        />
+        <CardsOutPlay
+          cardsInDeck={gameState.user.cardsInDeck}
+          lostCards={gameState.user.lostCards}
+          fraction={gameState.user.fraction}
+        />
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,102 +1,152 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import authRoute from './routes/auth.js'
-import cardsRoute from './routes/cards.js'
-import decksRoute from './routes/decks.js'
-import roomsRoute from './routes/rooms.js'
-import { Server } from 'socket.io'
-import http from 'http'
-import Room from './models/Room.js'
-import { randomCards } from './utils/randomCards.js'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoute from "./routes/auth.js";
+import cardsRoute from "./routes/cards.js";
+import decksRoute from "./routes/decks.js";
+import roomsRoute from "./routes/rooms.js";
+import { Server } from "socket.io";
+import http from "http";
+import { randomCards } from "./utils/randomCards.js";
 
-const app = express()
+const app = express();
 const server = http.createServer(app);
 
 dotenv.config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/auth', authRoute)
-app.use('/api/cards', cardsRoute)
-app.use('/api/decks', decksRoute)
-app.use('/api/rooms', roomsRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/cards", cardsRoute);
+app.use("/api/decks", decksRoute);
+app.use("/api/rooms", roomsRoute);
 
 const io = new Server(server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST'],
-    },
-  });
-  
-  const roomData = {
-    id: null,
-    gameState: []
-  };
-  
-  let currentPlayerIndex = 0;
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-  io.on('connection', (socket) => {
-    socket.on('join', ({ roomId, userData }) => {
-      roomData.id = roomId;
-  
-      const userIndex = roomData.gameState.findIndex((user) => user.username === userData.username);
-  
-      const currentCards = randomCards(userData.deck.cards);
-      const cardsInDeck = userData.deck.cards.filter((el) =>
-        currentCards.some((card) => card.name === el.name)
+const rooms = {};
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ roomId, userData }) => {
+    if (!rooms[roomId]) {
+      rooms[roomId] = {
+        id: roomId,
+        gameState: [],
+        currentPlayerIndex: 0,
+        players: [],
+      };
+    }
+
+    const room = rooms[roomId];
+    const userIndex = room.gameState.findIndex(
+      (user) => user.username === userData.username
+    );
+
+    if (!room.players.includes(socket.id)) {
+      room.players.push(socket.id);
+    }
+
+    const currentCards = randomCards(userData.deck.cards);
+    const cardsInDeck = userData.deck.cards.filter((el) =>
+      currentCards.some((card) => card.name === el.name)
+    );
+
+    if (userIndex === -1) {
+      room.gameState.push({
+        username: userData.username,
+        fraction: userData.deck.fraction.name,
+        cardsOnTable: [],
+        currentCards: currentCards,
+        cardsInDeck: cardsInDeck,
+        lostCards: [],
+        totalPower: 0,
+        lostRounds: 0,
+      });
+    }
+
+    socket.join(roomId);
+
+    if (room.gameState.length === 2) {
+      io.to(roomId).emit("gameState", { gameState: room.gameState });
+    }
+  });
+
+  socket.on("pushCard", ({ roomId, username, cardIndex }) => {
+    const room = rooms[roomId];
+    if (!room) return;
+
+    const userIndex = room.gameState.findIndex(
+      (user) => user.username === username
+    );
+
+    if (userIndex !== -1 && userIndex === room.currentPlayerIndex) {
+      const user = room.gameState[userIndex];
+      const card = user.currentCards[cardIndex];
+
+      user.currentCards.splice(cardIndex, 1);
+      user.cardsOnTable.push(card);
+      user.totalPower += card.power;
+
+      room.currentPlayerIndex =
+        (room.currentPlayerIndex + 1) % room.gameState.length;
+
+      const allPlayersOutOfCards = room.gameState.every(
+        (player) => player.currentCards.length === 0
       );
-  
-      if (userIndex === -1) {
-        roomData.gameState.push({
-          username: userData.username,
-          fraction: userData.deck.fraction.name,
-          cardsOnTable: [],
-          currentCards: currentCards,
-          cardsInDeck: cardsInDeck,
-          lostCards: [],
-          totalPower: 0,
-          lostRounds: 0,
-        });
-      }
-  
-      socket.join(roomId);
-  
-      if (roomData.gameState.length === 2) {
-        io.to(roomId).emit('gameState', { gameState: roomData.gameState });
-      }
-    });
-  
-    socket.on('pushCard', ({ username, cardIndex }) => {
-      const userIndex = roomData.gameState.findIndex((user) => user.username === username);
-      if (userIndex !== -1 && userIndex === currentPlayerIndex) {
-        const user = roomData.gameState[userIndex];
-        const card = user.currentCards[cardIndex];
-  
-        user.currentCards.splice(cardIndex, 1);
-        user.cardsOnTable.push(card);
-        user.totalPower += card.power;
-  
-        currentPlayerIndex = (currentPlayerIndex + 1) % 2;
-      }
-  
-      io.to(roomData.id).emit('gameState', { gameState: roomData.gameState });
-    });
-  
-    socket.on('disconnect', () => {
-    });
-  });
-  
 
+      if (allPlayersOutOfCards) {
+        const winner = room.gameState.reduce((prev, current) =>
+          prev.totalPower > current.totalPower ? prev : current
+        );
+
+        io.to(roomId).emit("gameOver", {
+          winner: winner.username,
+          gameState: room.gameState,
+        });
+
+        delete rooms[roomId];
+        return;
+      }
+    }
+
+    io.to(roomId).emit("gameState", { gameState: room.gameState });
+  });
+
+  socket.on("disconnect", () => {
+    for (const roomId in rooms) {
+      const room = rooms[roomId];
+      const playerIndex = room.players.indexOf(socket.id);
+
+      if (playerIndex !== -1) {
+        room.players.splice(playerIndex, 1);
+
+        if (room.players.length === 0) {
+          delete rooms[roomId];
+        }
+
+        break;
+      }
+    }
+  });
+});
 
 const start = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zustwai.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
-        server.listen(process.env.PORT, () => {console.log('Server started')})
-    } catch (err) {
-        console.log(err)
-    }
-}
-start()
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zustwai.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    server.listen(process.env.PORT, () => {
+      console.log("Server started");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
